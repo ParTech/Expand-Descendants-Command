88653b6bc4f0aadec9567feb1949fba051d715d3
Ruud van Falier <ruud@dottech.nl>
2013-07-29 20:27:25 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+Expand Descendants Command
+==========================
+
+Description
+-----------
+This module adds a command that allows the user to expand all descendants of an item from the Sitecore content tree.
+
+
+References
+------------
+Blog: http://www.partechit.nl/nl/blog/2013/03/handy-sitecore-command-expand-descendants
+GitHub: https://github.com/ParTech/Expand-Descendants-Command
+
+
+Installation
+------------
+The Sitecore package *\Release\ParTech.Commands.ExpandsDescendants-1.0.0.zip* contains:
+- Binary (release build).
+- Configuration include file.
+- Core items that are required for this module.
+
+Use the Sitecore Installation Wizard to install the package.
+After installation, the command will be available from the context menu in the Content Editor Tree.
+
+It will not require any further configuration.
+
+
+Release notes
+-------------
+*1.0.0*
+- Initial release
+
+
+Author
+------
+This solution was brought to you and supported by Ruud van Falier, ParTech IT
+
+Twitter: @BrruuD / @ParTechIT   
+E-mail: ruud@partechit.nl   
+Web: http://www.partechit.nl
---
+++ Source/ParTech.Commands.ExpandDescendants/ExpandDescendants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Data.Items;
+using Sitecore.Shell.Framework.Commands;
+using Sitecore.Web.UI.Sheer;
+using Sitecore.Globalization;
+
+namespace ParTech.Commands
+{
+    /// <summary>
+    /// Command that expands all the descendants of the selected item in the Content Editor tree
+    /// </summary>
+    public class ExpandDescendants : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Item parent = context.Items.FirstOrDefault();
+
+            RefreshChildren(parent);
+        }
+
+        private void RefreshChildren(Item parent)
+        {
+            if (parent != null)
+            {
+                Sitecore.Context.ClientPage.SendMessage(this, string.Format("item:refreshchildren(id={0})", parent.ID));
+
+                var children = parent.GetChildren();
+
+                foreach (Item child in children)
+                {
+                    RefreshChildren(child);
+                }
+            }
+        }
+    }
+}
